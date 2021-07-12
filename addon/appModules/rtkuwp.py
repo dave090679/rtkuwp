1487e010b414daf5b48bd444fdd5d34814130429
David Parduhn <xkill85@gmx.net>
2021-07-12 13:20:46 +0200

    Aktualisierung auf NVDA 2021.1

---
+++ addon/appModules/rtkuwp.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+#appModules/avpui.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2012 NVDA Contributors
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+import appModuleHandler
+import api
+import controlTypes
+from NVDAObjects.UIA import ListItem, UIA
+class rtkuwpuia(UIA):
+	def _get_name(self):
+		return self.UIAElement.CurrentAutomationId
+
+
+class rtkuwplistitem(ListItem):
+	def _get_name(self):
+		l = list()
+		for x in self.children:
+			childname = x.name
+			childrole = x.role
+			if childname and childrole == controlTypes.ROLE_STATICTEXT:
+				l.append(childname) 
+		return ', '.join(l)
+
+class AppModule(appModuleHandler.AppModule):
+	def chooseNVDAObjectOverlayClasses(self, obj, clslist):
+		if obj.role == controlTypes.ROLE_LISTITEM:
+			clslist.insert(0,rtkuwplistitem)
+		elif obj.name == '':
+			clslist.insert(0,rtkuwpuia)
--- buildVars.py
+++ buildVars.py
@@ -11,22 +11,23 @@ addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
 	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
 	# add-on Name, internal for nvda
-	"addon_name" : "addonTemplate",
+	"addon_name" : "rtkuwp",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("Add-on user visible name"),
+	"addon_summary" : _("realtek audio console"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Description for the add-on.
-It can span multiple lines."""),
+	"addon_description" : _("""verbessert die Zugaenglichkeit der Realtek audio console fuer windows 10
+"""),
 	# version
-	"addon_version" : "x.y",
+	"addon_version" : "0.4",
 	# Author(s)
-	"addon_author" : u"name <name@domain.com>",
+	"addon_author" : u"David Parduhn <xkill85@gmx.net>",
 	# URL for the add-on documentation support
 	"addon_url" : None,
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
+	"lastTestedNVDAVersion": "2021.1",
 }
 
 
